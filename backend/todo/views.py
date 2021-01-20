98acf63d7f9b5ebb3ce6a4917b4600d211c84c5a
ibnuirfan <ibnuirfan09@gmail.com>
2021-01-20 09:47:18 +0800

    update the folder

---
+++ backend/todo/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .seralizers import TodoSerializers
+from .models import Todo
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializers
+    queryset = Todo.objects.all()
